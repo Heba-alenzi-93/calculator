0c06b5b62a39e01499b5fcb5e276d73733c50d1a
apple <apple@apples-MacBook-Pro.local>
2022-07-04 19:46:54 +0300

    i did the task

--- calculator.py
+++ calculator.py
@@ -1,7 +1,37 @@
 
+def get_calculation (num1,num2,operation):
+	if operation == "+":
+		print(f"The answer is {int(num1)+ int(num2)}")
+	elif operation == "-":
+		print(f"The answer is {int(num1) - int(num2)}")	
+	elif operation == "/":
+		print(f"The answer is {int(num1)/ int(num2)}")
+	elif operation == "*":
+		print(f"The answer is {int(num1)* int(num2)}")
+	else:
+		return None
+
+
 def main():
-	#write your code here
-	pass
+	num1= input("Enter the first number:")
+	num2=input("Enter the second number:")
+	
+	if num1.isdigit() and num2.isdigit():
+		operation=input("Choose the operation (+, -, /, *):") 
+		answer = get_calculation(num1,num2,operation)
+		if answer == None:
+			print("The operation is not valid ")
+		else:
+			print("the answer is "+ str(answer))
+	else:
+		print("The input is not number ")
+
+
+
+		
+		
+
+
 	
 
 
